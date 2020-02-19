ea1595303780e5a827cc854d14c02f42890e2248
Artur Kulmukhametov <artur@artur.com>
2020-02-19 14:30:41 +0100

    the fits client finally works

--- fits-client/src/main/java/at/ac/tuwien/ifs/FITSClient.java
+++ fits-client/src/main/java/at/ac/tuwien/ifs/FITSClient.java
@@ -1,56 +1,69 @@
 package at.ac.tuwien.ifs;
 
 
-import at.ac.tuwien.ifs.requests.FitsProcessFileRequest;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class FITSClient {
+    private String FITS_HOST = "ttp://localhost:8080/";
 
-    public String getVersion() {
-        Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/version");
-        Response response = target.request(MediaType.APPLICATION_JSON).get();
-        int status = response.getStatus();
-        if (status > 200) {
+    public String getVersion() throws IOException {
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(FITS_HOST + "version");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+        if (statusCode > 200) {
             response.close();
-            throw new WebApplicationException(status);
+            throw new WebApplicationException(responseBody, statusCode);
         }
-        String value = response.readEntity(String.class);
-        response.close();
-        return value;
+
+        return responseBody;
     }
 
     public String processFile(File file) throws IOException {
-        Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/fits/examine");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
 
+        HttpPost httppost = new HttpPost(FITS_HOST + "examine");
+        FileBody bin = new FileBody(file);
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addPart("datafile", bin);
+        HttpEntity reqEntity = builder.build();
+        httppost.setEntity(reqEntity);
 
-        MultipartFormDataOutput mdo = new MultipartFormDataOutput();
-        mdo.addFormData("datafile", Files.readAllBytes(file.toPath()), MediaType.TEXT_PLAIN_TYPE);
-        GenericEntity<MultipartFormDataOutput> entity = new GenericEntity<MultipartFormDataOutput>(mdo) { };
-        Response response = target.request().post(Entity.entity(entity, MediaType.MULTIPART_FORM_DATA_TYPE));
-        int status = response.getStatus();
-        if (status > 200) {
+        CloseableHttpResponse response = httpclient.execute(httppost);
+        int statusCode = response.getStatusLine().getStatusCode();
+        String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+        if (statusCode > 200) {
             response.close();
-            throw new WebApplicationException(status);
+            throw new WebApplicationException(responseBody, statusCode);
         }
-        String value = response.readEntity(String.class);
-        response.close();
-        return value;
+
+        return responseBody;
+
     }
 
 
+    public String getFITS_HOST() {
+        return FITS_HOST;
+    }
+
+    public void setFITS_HOST(String FITS_HOST) {
+        this.FITS_HOST = FITS_HOST;
+    }
 }
--- fits-client/src/test/java/at/ac/tuwien/ifs/FITSClientTest.java
+++ fits-client/src/test/java/at/ac/tuwien/ifs/FITSClientTest.java
@@ -4,11 +4,13 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockserver.integration.ClientAndServer;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
@@ -17,9 +19,51 @@ import static org.mockserver.model.HttpResponse.response;
 class FITSClientTest {
     private ClientAndServer mockServer;
 
+    private int MOCK_SERVER_PORT = 8088;
+
+    private String VALID_FITS_RESULT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<fits xmlns=\"http://hul.harvard.edu/ois/xml/ns/fits/fits_output\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://hul.harvard.edu/ois/xml/ns/fits/fits_output http://hul.harvard.edu/ois/xml/xsd/fits/fits_output.xsd\" version=\"1.5.0\" timestamp=\"2/19/20 1:26 PM\">\n" +
+            "  <identification status=\"CONFLICT\">\n" +
+            "    <identity format=\"Markdown\" mimetype=\"text/markdown\" toolname=\"FITS\" toolversion=\"1.5.0\">\n" +
+            "      <tool toolname=\"Droid\" toolversion=\"6.4\" />\n" +
+            "      <externalIdentifier toolname=\"Droid\" toolversion=\"6.4\" type=\"puid\">fmt/1149</externalIdentifier>\n" +
+            "    </identity>\n" +
+            "    <identity format=\"Plain text\" mimetype=\"text/plain\" toolname=\"FITS\" toolversion=\"1.5.0\">\n" +
+            "      <tool toolname=\"Jhove\" toolversion=\"1.20.1\" />\n" +
+            "      <tool toolname=\"file utility\" toolversion=\"5.35\" />\n" +
+            "    </identity>\n" +
+            "  </identification>\n" +
+            "  <fileinfo>\n" +
+            "    <size toolname=\"Jhove\" toolversion=\"1.20.1\">903</size>\n" +
+            "    <filepath toolname=\"OIS File Information\" toolversion=\"1.0\" status=\"SINGLE_RESULT\">/usr/local/tomcat/webapps/fits/upload/1582118786085/README.md</filepath>\n" +
+            "    <filename toolname=\"OIS File Information\" toolversion=\"1.0\" status=\"SINGLE_RESULT\">README.md</filename>\n" +
+            "    <md5checksum toolname=\"OIS File Information\" toolversion=\"1.0\" status=\"SINGLE_RESULT\">133c6cf05a139fa2e472ce6fa11bb5d2</md5checksum>\n" +
+            "    <fslastmodified toolname=\"OIS File Information\" toolversion=\"1.0\" status=\"SINGLE_RESULT\">1582118786000</fslastmodified>\n" +
+            "  </fileinfo>\n" +
+            "  <filestatus />\n" +
+            "  <metadata />\n" +
+            "  <statistics fitsExecutionTime=\"178\">\n" +
+            "    <tool toolname=\"MediaInfo\" toolversion=\"0.7.75\" status=\"did not run\" />\n" +
+            "    <tool toolname=\"OIS Audio Information\" toolversion=\"0.1\" status=\"did not run\" />\n" +
+            "    <tool toolname=\"ADL Tool\" toolversion=\"0.1\" status=\"did not run\" />\n" +
+            "    <tool toolname=\"VTT Tool\" toolversion=\"0.1\" status=\"did not run\" />\n" +
+            "    <tool toolname=\"Droid\" toolversion=\"6.4\" executionTime=\"10\" />\n" +
+            "    <tool toolname=\"Jhove\" toolversion=\"1.20.1\" executionTime=\"42\" />\n" +
+            "    <tool toolname=\"file utility\" toolversion=\"5.35\" executionTime=\"65\" />\n" +
+            "    <tool toolname=\"Exiftool\" toolversion=\"11.54\" executionTime=\"176\" />\n" +
+            "    <tool toolname=\"NLNZ Metadata Extractor\" toolversion=\"3.6GA\" status=\"did not run\" />\n" +
+            "    <tool toolname=\"OIS File Information\" toolversion=\"1.0\" executionTime=\"5\" />\n" +
+            "    <tool toolname=\"OIS XML Metadata\" toolversion=\"0.2\" status=\"did not run\" />\n" +
+            "    <tool toolname=\"ffident\" toolversion=\"0.2\" executionTime=\"14\" />\n" +
+            "    <tool toolname=\"Tika\" toolversion=\"1.21\" executionTime=\"69\" />\n" +
+            "  </statistics>\n" +
+            "</fits>\n" +
+            "\n" +
+            "\n";
+
     @BeforeEach
     void setUp() {
-        mockServer = mockServer.startClientAndServer(8088);
+        mockServer = mockServer.startClientAndServer(MOCK_SERVER_PORT);
     }
 
     @AfterEach
@@ -27,10 +71,8 @@ class FITSClientTest {
         mockServer.stop();
     }
 
-
     @Test
-    void getVersionTest() {
-
+    void getVersionTest() throws IOException {
         mockServer.when(
                 request()
                         .withMethod("GET")
@@ -44,22 +86,47 @@ class FITSClientTest {
 
 
         FITSClient fitsClient = new FITSClient();
+        fitsClient.setFITS_HOST(String.format("http://localhost:%d/", MOCK_SERVER_PORT));
+
         String s = fitsClient.getVersion();
-
-
         Assert.assertEquals("1.5.0", s);
-
     }
 
 
     @Test
     void processFileTest() throws IOException {
 
+        mockServer.when(
+                request()
+                        .withMethod("POST")
+                        .withPath("/fits/examine")
+                        .withHeader("\"Content-type\", \"application/json\""))
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withBody(VALID_FITS_RESULT)
+                );
+
         FITSClient fitsClient = new FITSClient();
-        String s = fitsClient.processFile(new File("/Users/artur/rnd/git/c3po/README.md"));
+        fitsClient.setFITS_HOST(String.format("http://localhost:%d/fits/", 8088));
+
+        URL resource = getClass().getClassLoader().getResource("README.md");
+        String output = fitsClient.processFile(new File(resource.getPath()));
+
+        Assert.assertEquals(VALID_FITS_RESULT, output);
+    }
 
 
-        Assert.assertEquals("1.5.0", s);
+    //The test can be run against running FITS service, i.e. fits-docker
+    @Disabled
+    @Test
+    void processFileTestWithoutMock() throws IOException {
+        FITSClient fitsClient = new FITSClient();
+        fitsClient.setFITS_HOST(String.format("http://localhost:%d/fits/", 8080));
 
+        URL resource = getClass().getClassLoader().getResource("README.md");
+        String output = fitsClient.processFile(new File(resource.getPath()));
+
+        Assert.assertTrue(output.contains("Tika"));
     }
 }
