252b38d2243a6c8d36dd928f404c8a197c1d702e
artourkin <artur.kulmukhametov@tuwien.ac.at>
2020-02-19 09:02:59 +0100

    adding a multipart request

--- fits-client/src/main/java/at/ac/tuwien/ifs/FITSClient.java
+++ fits-client/src/main/java/at/ac/tuwien/ifs/FITSClient.java
@@ -2,14 +2,17 @@ package at.ac.tuwien.ifs;
 
 
 import at.ac.tuwien.ifs.requests.FitsProcessFileRequest;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -32,18 +35,13 @@ public class FITSClient {
 
     public String processFile(File file) throws IOException {
         Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/examine");
-
-        MultiPart multiPart = new MultiPart();
+        WebTarget target = client.target("http://localhost:8080/fits/examine");
 
 
-
-        byte[] fileContent = Files.readAllBytes(file.toPath());
-
-        FitsProcessFileRequest request = new FitsProcessFileRequest();
-        request.setDatafile(fileContent);
-        Response response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(request,
-                MediaType.MULTIPART_FORM_DATA));
+        MultipartFormDataOutput mdo = new MultipartFormDataOutput();
+        mdo.addFormData("datafile", Files.readAllBytes(file.toPath()), MediaType.TEXT_PLAIN_TYPE);
+        GenericEntity<MultipartFormDataOutput> entity = new GenericEntity<MultipartFormDataOutput>(mdo) { };
+        Response response = target.request().post(Entity.entity(entity, MediaType.MULTIPART_FORM_DATA_TYPE));
         int status = response.getStatus();
         if (status > 200) {
             response.close();
