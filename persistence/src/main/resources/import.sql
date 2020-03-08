d9fc8d6376daa1e400abbf61da8856c1bff056f7
artourkin <artur.kulmukhametov@tuwien.ac.at>
2020-03-08 22:43:02 +0100

    Fixed broken tests

--- persistence/src/main/resources/import.sql
+++ persistence/src/main/resources/import.sql
@@ -1,11 +1,4 @@
-INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (1, '/home/artur','FORMAT',
-'DROID:3', 'pdf', 'STRING');
-INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (2, '/home/artur',
-'MIMETYPE',
-'DROID:3', 'application/pdf', 'STRING');
-INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (3, '/home/artur',
-'FORMAT_VERSION',
-'DROID:3', '1.4', 'STRING');
-
-INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (4, '/home/artur2','FORMAT',
-'DROID:3', 'doc', 'STRING');
+INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (1, '/home/artur','FORMAT','DROID:3', 'pdf', 'STRING');
+INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (2, '/home/artur','MIMETYPE','DROID:3', 'application/pdf', 'STRING');
+INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (3, '/home/artur','FORMAT_VERSION','DROID:3', '1.4', 'STRING');
+INSERT INTO CharacterisationResult(id, filePath, property, source, value, valueType) VALUES (4, '/home/artur2','FORMAT','DROID:3', 'doc', 'STRING');
