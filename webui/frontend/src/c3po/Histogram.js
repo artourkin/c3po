0015eca54ac81cf09c59a148262db13a41677b36
artourkin <artur.kulmukhametov@tuwien.ac.at>
2020-06-27 08:43:53 +0200

    First time the histogram works

--- webui/frontend/src/c3po/Histogram.js
+++ webui/frontend/src/c3po/Histogram.js
@@ -1,22 +1,54 @@
 import React, { Component } from "react";
 import { Bar, Line } from "react-chartjs-2";
 
+const bar = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+  ],
+};
+
 class Histogram extends Component {
+
+
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: bar
     };
   }
 
   componentDidMount() {
     fetch("http://localhost:8081/rest/properties")
       .then(response => response.json())
-      .then(data => this.setState({ data }));
+      .then(data =>
+
+      {
+        console.log(data);
+        data = this.transform(data);
+        this.setState({ data })
+      });
   }
+
+  transform(data) {
+    //bar.labels = data.
+    bar.datasets[0].data = data.map(item =>item.count);
+    bar.data = data.map(item =>item.count);
+    bar.labels=data.map(item =>item.property);
+    return bar;
+  }
+
   render() {
     const { hits } = this.state;
-    return <Bar data={this.state.data} height={70} />;
+    return <Bar data={bar} height={170} />;
   }
 }
 
